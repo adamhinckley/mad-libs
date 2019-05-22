489b256cca7f07dbf0a16d5547b46787625a0cfc
adamhinckley <adamhinckley@mac.com>
2019-05-21 22:54:03 -0600

    basic foundation is working, but there are some small bugs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
 import React from "react";
 import "./App.css";
+import { madlib } from "./madLibsText/madlibs";
+
+const { blanks, value, title } = madlib;
+const solution = [];
 
 function App() {
-    const [words, setWords] = React.useState([]);
+    const [words] = React.useState([]);
     const [input, setInput] = React.useState("");
-
-    console.log(input);
-    console.log(words);
+    let [count, setCount] = React.useState(13);
+    // const [message, setMessage] = React.useState("");
+    const [complete, setComplete] = React.useState(false);
 
     const changeHandler = e => {
         setInput(e.target.value);
     };
 
-    const addWords = (e, word) => {
+    const addWords = (e, word, blanks) => {
         e.preventDefault();
         if (word.length > 0) {
             words.push(word);
+            incrementBlanks(blanks, words, value);
         }
         setInput("");
-        console.log("word array", words);
+    };
+
+    const incrementBlanks = (blanks, words, value) => {
+        let max = blanks.length;
+
+        if (count < max) {
+            count++;
+            setCount(count);
+        } else {
+            setComplete(true);
+            combineBlanksAndValues(value, words);
+        }
+        console.log(solution);
+    };
+
+    const combineBlanksAndValues = (value, words) => {
+        for (let i = 0; i < value.length; i++) {
+            solution.push(value[i]);
+            if (words) {
+                solution.push(words[i]);
+            }
+        }
+        return solution;
     };
 
     return (
-        <form onSubmit={e => addWords(e, input)}>
-            <label htmlFor="">Label</label>
-            <input type="text" placeholder="Add a word" name="input" onChange={changeHandler} value={input} />
-            <button onClick={e => addWords(e, input)}>add</button>
-        </form>
+        <div>
+            <h1>{title}</h1>
+            <form onSubmit={e => addWords(e, input, blanks)}>
+                <label htmlFor="">{blanks[count]}</label>
+                <input
+                    type="text"
+                    placeholder="Add a word"
+                    name="input"
+                    onChange={changeHandler}
+                    value={input}
+                />
+                <button>add</button>
+                {complete === true
+                    ? (console.log("Solution in return", solution),
+                      solution.map(solution => {
+                          return <p>{solution}</p>;
+                      }))
+                    : null}
+            </form>
+        </div>
     );
 }
 
